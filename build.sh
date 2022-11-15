a012f6c7d7d9e26db71f492ff8004534861d51f5
Hipolyto <polo@letsolutions.com.mx>
2022-11-14 20:17:21 -0700

    cleaning repo

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sh carthage.sh update --use-xcframeworks 
-
-xcodebuild archive -sdk iphoneos -project FacebookProxy.xcodeproj -scheme FacebookProxy -configuration Release -archivePath Output/Output-iphoneos SKIP_INSTALL=NO
-xcodebuild archive -sdk iphonesimulator -project FacebookProxy.xcodeproj -scheme FacebookProxy -configuration Release -archivePath Output/Output-iphonesimulator SKIP_INSTALL=NO
-
-xcodebuild -create-xcframework -framework Output/Output-iphonesimulator.xcarchive/Products/Library/Frameworks/FacebookProxy.framework -framework Output/Output-iphoneos.xcarchive/Products/Library/Frameworks/FacebookProxy.framework -output Output/FacebookProxy.xcframework
