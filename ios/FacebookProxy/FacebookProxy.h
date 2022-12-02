fee5528d8cdd57b2beb1fde446df0722f1211edf
thisisthekap <christian.kapplmueller@tonestro.com>
2022-12-02 09:25:54 +0100

    cleared obsolete namings

--- ios/FacebookProxy/FacebookProxy.h
+++ ios/FacebookProxy/FacebookProxy.h
@@ -7,6 +7,6 @@ FOUNDATION_EXPORT double FacebookProxyVersionNumber;
 
 FOUNDATION_EXPORT const unsigned char FacebookProxyVersionNumber[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <NukeProxy/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <FacebookProxy/PublicHeader.h>
 
 
